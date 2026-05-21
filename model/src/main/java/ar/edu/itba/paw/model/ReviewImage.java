9c129316f32c81388e604f97457f9a9f2c7dd6f0
Rocco Perrone <theroccoperrone@gmail.com>
2026-05-21 16:15:08 -0300

    feat(model): add ReviewImage entity

---
+++ model/src/main/java/ar/edu/itba/paw/model/ReviewImage.java
@@ -0,0 +1,93 @@
+package ar.edu.itba.paw.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "review_images")
+public class ReviewImage implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private long imageId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(name = "display_order")
+    private int displayOrder;
+
+    @Column(name = "content_type")
+    private String contentType;
+
+    @Column(name = "image_data")
+    private byte[] imageData;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    public ReviewImage() {}
+
+    public long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(final long imageId) {
+        this.imageId = imageId;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void setReview(final Review review) {
+        this.review = review;
+    }
+
+    public long getReviewId() {
+        return review != null ? review.getId() : 0;
+    }
+
+    public int getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(final int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(final String contentType) {
+        this.contentType = contentType;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(final byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(final LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
